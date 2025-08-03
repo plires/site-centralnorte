1e9437ef1b9b3efcca08316607c2bb6c137e0e26
Pablo Lires <pablo@librecomunicacion.net>
2025-08-03 18:45:19 -0300

    Update web.php. Cambio en parametro de ruta para que Laravel cargue el recurso

--- routes/web.php
+++ routes/web.php
@@ -33,10 +33,10 @@ Route::middleware(['auth', 'verified', 'permission:gestionar_roles'])->prefix('d
     Route::get('/roles', [RoleController::class, 'index'])->name('roles.index');
     Route::get('/roles/create', [RoleController::class, 'create'])->name('roles.create');
     Route::post('/roles', [RoleController::class, 'store'])->name('roles.store');
-    Route::get('/roles/{rol}', [RoleController::class, 'show'])->name('roles.show');
-    Route::get('/roles/{rol}/edit', [RoleController::class, 'edit'])->name('roles.edit');
-    Route::put('/roles/{rol}', [RoleController::class, 'update'])->name('roles.update');
-    Route::delete('/roles/{rol}', [RoleController::class, 'destroy'])->name('roles.destroy');
+    Route::get('/roles/{role}', [RoleController::class, 'show'])->name('roles.show');
+    Route::get('/roles/{role}/edit', [RoleController::class, 'edit'])->name('roles.edit');
+    Route::put('/roles/{role}', [RoleController::class, 'update'])->name('roles.update');
+    Route::delete('/roles/{role}', [RoleController::class, 'destroy'])->name('roles.destroy');
 });
 
 require __DIR__ . '/settings.php';
