6629c0a4437caa380b8cf99de57c0dc3bb694779
Pablo Lires <pablolires@gmail.com>
2025-08-08 12:41:02 -0300

    Agregado de campo description a tabla categories

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -12,7 +12,7 @@ class Category extends Model
     /** @use HasFactory<\Database\Factories\CategoryFactory> */
     use HasFactory, SoftDeletes;
 
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'description'];
 
     public function products()
     {
--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -18,6 +18,7 @@ class CategoryFactory extends Factory
     {
         return [
             'name' => $this->faker->word,
+            'description' => $this->faker->sentence(),
         ];
     }
 }
