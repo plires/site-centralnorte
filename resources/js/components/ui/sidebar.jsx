fea9cef77f8ec343c61750e6e129294851946cf1
Pablo Lires <pablo@librecomunicacion.net>
2025-11-19 15:41:22 -0300

    Update sidebar.jsx. Cambio de z-index en sidebar

--- resources/js/components/ui/sidebar.jsx
+++ resources/js/components/ui/sidebar.jsx
@@ -125,7 +125,7 @@ function Sidebar({ side = "left", variant = "sidebar", collapsible = "offcanvas"
       <div className={cn("relative h-svh w-(--sidebar-width) bg-transparent transition-[width] duration-200 ease-linear", "group-data-[collapsible=offcanvas]:w-0", "group-data-[side=right]:rotate-180", variant === "floating" || variant === "inset"
             ? "group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)+(--spacing(4)))]"
             : "group-data-[collapsible=icon]:w-(--sidebar-width-icon)")}/>
-      <div className={cn("fixed inset-y-0 z-10 hidden h-svh w-(--sidebar-width) transition-[left,right,width] duration-200 ease-linear md:flex", side === "left"
+      <div className={cn("fixed inset-y-0 z-11 hidden h-svh w-(--sidebar-width) transition-[left,right,width] duration-200 ease-linear md:flex", side === "left"
             ? "left-0 group-data-[collapsible=offcanvas]:left-[calc(var(--sidebar-width)*-1)]"
             : "right-0 group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)]", 
         // Adjust the padding for floating and inset variants.
