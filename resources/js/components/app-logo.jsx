db1c214ac0600833e9064bda81675f94c20cbf72
Pablo Lires <pablolires@gmail.com>
2025-12-05 17:06:54 -0300

    Update app-logo.jsx

--- resources/js/components/app-logo.jsx
+++ resources/js/components/app-logo.jsx
@@ -5,7 +5,7 @@ export default function AppLogo() {
     return (
         <>
             <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
-                <img src={product.placeholder_image} alt="Placeholder" />
+                <img className="rounded-md" src={product.placeholder_image} alt="Placeholder" />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
                 <span className="mb-0.5 truncate leading-none font-semibold">{name}</span>
