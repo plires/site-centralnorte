de696fb021a30bf8787002fde0c4500b60d00aeb
Pablo Lires <pablolires@gmail.com>
2026-02-26 18:45:40 -0300

    Reordenamiento de /teamMembers.js en page /nosotros

--- resources/js/pages/public/site/nosotros/data/teamMembers.js
+++ resources/js/pages/public/site/nosotros/data/teamMembers.js
@@ -1,36 +1,36 @@
 // Importar imágenes del team
-import member1 from '@/../images/nosotros/team/team-1.webp';
-import member10 from '@/../images/nosotros/team/team-10.webp';
-import member11 from '@/../images/nosotros/team/team-11.webp';
-import member12 from '@/../images/nosotros/team/team-12.webp';
-import member13 from '@/../images/nosotros/team/team-13.webp';
-import member14 from '@/../images/nosotros/team/team-14.webp';
-import member15 from '@/../images/nosotros/team/team-15.webp';
-import member2 from '@/../images/nosotros/team/team-2.webp';
-import member3 from '@/../images/nosotros/team/team-3.webp';
-import member4 from '@/../images/nosotros/team/team-4.webp';
-import member5 from '@/../images/nosotros/team/team-5.webp';
-import member6 from '@/../images/nosotros/team/team-6.webp';
-import member7 from '@/../images/nosotros/team/team-7.webp';
-import member8 from '@/../images/nosotros/team/team-8.webp';
-import member9 from '@/../images/nosotros/team/team-9.webp';
+import member12 from '@/../images/nosotros/team/agustin-gallardo.webp';
+import member8 from '@/../images/nosotros/team/agustina-bazterrechea.webp';
+import member7 from '@/../images/nosotros/team/alan-parodi.webp';
+import member4 from '@/../images/nosotros/team/andrea-ricco.webp';
+import member15 from '@/../images/nosotros/team/carmela-rojas.webp';
+import member2 from '@/../images/nosotros/team/cristian-quinteros.webp';
+import member6 from '@/../images/nosotros/team/daiana-candia.webp';
+import member11 from '@/../images/nosotros/team/juan-cruz-tonelli.webp';
+import member9 from '@/../images/nosotros/team/martina-niszhang.webp';
+import member1 from '@/../images/nosotros/team/matias-torres.webp';
+import member3 from '@/../images/nosotros/team/sergio-rodriguez.webp';
+import member5 from '@/../images/nosotros/team/soledad-trello.webp';
+import member13 from '@/../images/nosotros/team/tania-gomez.webp';
+import member10 from '@/../images/nosotros/team/valentin-bermudez.webp';
+import member14 from '@/../images/nosotros/team/victor-guaido.webp';
 
 const teamMembers = [
-    { image: member1, name: 'Matias Torres', role: 'Ejecutivo de Cuentas' },
-    { image: member2, name: 'Cristian Quinteros', role: 'Producción' },
-    { image: member3, name: 'Sergio Rodriguez', role: 'Gerente de RRHH' },
-    { image: member4, name: 'Andrea Ricco', role: 'Dirección' },
-    { image: member5, name: 'Soledad Tello', role: 'Ejecutiva de Cuentas' },
-    { image: member6, name: 'Daiana Candia', role: 'Coordinadora de Ventas' },
-    { image: member7, name: 'Alan Parodi', role: 'Producción' },
-    { image: member8, name: 'Agustina Bazterrechea', role: 'Marketing' },
-    { image: member9, name: 'Martina Niszhang', role: 'Ejecutiva de Ventas' },
-    { image: member10, name: 'Valentín Bernuez', role: 'Marketing' },
-    { image: member11, name: 'Juan Cruz Tonelli', role: 'Operaciones' },
     { image: member12, name: 'Agustín Gallardo', role: 'Administración' },
+    { image: member8, name: 'Agustina Bazterrechea', role: 'Marketing' },
+    { image: member7, name: 'Alan Parodi', role: 'Producción' },
+    { image: member4, name: 'Andrea Ricco', role: 'Dirección' },
+    { image: member15, name: 'Carmela Rojas', role: 'Marketing' },
+    { image: member2, name: 'Cristian Quinteros', role: 'Operaciones' },
+    { image: member6, name: 'Daiana Candia', role: 'Coordinadora de Ventas' },
+    { image: member11, name: 'Juan Cruz Tonelli', role: 'Operaciones' },
+    { image: member9, name: 'Martina Niszhang', role: 'Ejecutiva de Cuentas' },
+    { image: member1, name: 'Matias Torres', role: 'Ejecutivo de Cuentas' },
+    { image: member3, name: 'Sergio Rodriguez', role: 'Gerente de RRHH' },
+    { image: member5, name: 'Soledad Tello', role: 'Ejecutiva de Cuentas' },
     { image: member13, name: 'Tania Gomez', role: 'Marketing' },
+    { image: member10, name: 'Valentín Bernuez', role: 'Marketing' },
     { image: member14, name: 'Victor Guaido', role: 'Operaciones' },
-    { image: member15, name: 'Carmela Rojas', role: 'Diseñadora Gráfica' },
 ];
 
 export default teamMembers;
