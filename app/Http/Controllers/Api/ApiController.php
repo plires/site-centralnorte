72a5f67ff7c8550b602b53d22dc04d7b045d21f7
Pablo Lires <pablolires@gmail.com>
2026-02-26 09:49:56 -0300

    Update ApiController.php. Se elimina importacion que ya no se utiliza

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -8,7 +8,6 @@ use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
 
 class ApiController extends Controller
 {
